ef689f958790d3243385d770244c4e67a8e3add3
wjrwjr <1966336874@qq.com>
2023-03-02 16:59:49 +0800

    upd

--- vector.h
+++ vector.h
@@ -1587,54 +1587,77 @@ constexpr size_t _Get_max_bytes_num() {
 	else return 1;
 }
 
-template<typename _Ty>
+template<size_t C, typename _Ty>
 size_t _Get_bytes_num(const _Ty& val) {
 	constexpr size_t N = sizeof(_Ty);
-	constexpr size_t M = _Get_max_bytes_num<N>();
+	constexpr size_t M = _Get_max_bytes_num<C>();
 	const auto ptr = reinterpret_cast<const uint8_t*>(&val);
-	if constexpr (N == 1) {
-		return 1;
-	}
-	else if constexpr (N == 2) {
-		return (ptr[0] == ptr[1]) ? 1 : 2;
-	}
-	else if constexpr (N == 3) {
-		return (*reinterpret_cast<const uint16_t*>(ptr) == *reinterpret_cast<const uint16_t*>(ptr + 1)) ? 1 : 0;
-	}
-	else if constexpr (N == 4) {
-		auto x = *reinterpret_cast<const uint32_t*>(ptr);
-		return ((x >> 8) == (x & 0x00FFFFFF)) ? 1 : 4;
-	}
-	else if constexpr (N == 5) {
-		return (*reinterpret_cast<const uint32_t*>(ptr) == *reinterpret_cast<const uint32_t*>(ptr + 1)) ? 1 : 0;
-	}else if constexpr(N == 6){
-		return (*reinterpret_cast<const uint32_t*>(ptr) == *reinterpret_cast<const uint32_t*>(ptr + 2)) ? 2 : 6;
-	}
-	else if constexpr (N == 7) {
-		auto x = *reinterpret_cast<const uint32_t*>(ptr);
-		auto y = *reinterpret_cast<const uint32_t*>(ptr + 4);
-		return ((x >> 8) == (x & 0x00FFFFFF)) && ((y >> 8) == (y & 0x00FFFFFF)) ? 1 : 0;
-	}
-	else if constexpr(N == 8) {
-		auto x = *reinterpret_cast<const uint64_t*>(ptr);
-		return ((x >> 8) == (x & 0x00FFFFFFFFFFFFFF)) ? 1 : 8;
-	}
-	else {
-		if (!is_constant_p(val)) {
-			return 0;
+	if constexpr (N == C) {
+		if constexpr (N == 1) {
+			return 1;
+		}
+		else if constexpr (N == 2) {
+			return (ptr[0] == ptr[1]) ? 1 : 2;
+		}
+		else if constexpr (N == 3) {
+			return (*reinterpret_cast<const uint16_t*>(ptr) == *reinterpret_cast<const uint16_t*>(ptr + 1)) ? 1 : 0;
+		}
+		else if constexpr (N == 4) {
+			auto x = *reinterpret_cast<const uint32_t*>(ptr);
+			return ((x >> 8) == (x & 0x00FFFFFF)) ? 1 : 4;
+		}
+		else if constexpr (N == 5) {
+			return (*reinterpret_cast<const uint32_t*>(ptr) == *reinterpret_cast<const uint32_t*>(ptr + 1)) ? 1 : 0;
+		}
+		else if constexpr (N == 6) {
+			return (*reinterpret_cast<const uint32_t*>(ptr) == *reinterpret_cast<const uint32_t*>(ptr + 2)) ? 2 : 6;
+		}
+		else if constexpr (N == 7) {
+			auto x = *reinterpret_cast<const uint32_t*>(ptr);
+			auto y = *reinterpret_cast<const uint32_t*>(ptr + 4);
+			return ((x >> 8) == (x & 0x00FFFFFF)) && ((y >> 8) == (y & 0x00FFFFFF)) ? 1 : 0;
+		}
+		else if constexpr (N == 8) {
+			auto x = *reinterpret_cast<const uint64_t*>(ptr);
+			return ((x >> 8) == (x & 0x00FFFFFFFFFFFFFF)) ? 1 : 8;
 		}
 		else {
-			for (size_t i = 0; i < N - M; ++i) {
-				if (ptr[i] != ptr[i + M]) {
-					return 0;
-				}
+			if (!is_constant_p(val)) {
+				return 0;
 			}
-			for (size_t i = 0; i < M - 1; ++i) {
-				if (ptr[i] != ptr[i + 1]) {
-					return M;
+			else {
+				for (size_t i = 0; i < N - M; ++i) {
+					if (ptr[i] != ptr[i + M]) {
+						return 0;
+					}
 				}
+				for (size_t i = 0; i < M - 1; ++i) {
+					if (ptr[i] != ptr[i + 1]) {
+						return M;
+					}
+				}
+				return 1;
 			}
-			return 1;
+		}
+	}
+	else {
+		// all zeros
+		if constexpr (N <= 8) {
+			constexpr uint8_t zero[8] = { 0,0,0,0,0,0,0,0 };
+			return ::memcmp(ptr, zero, N) == 0 ? 1 : 0;
+		}
+		else {
+			if (!is_constant_p(val)) {
+				return 0;
+			}
+			else {
+				for (size_t i = 0; i < N; ++i) {
+					if (ptr[i] != 0) {
+						return 0;
+					}
+				}
+				return 1;
+			}
 		}
 	}
 }
@@ -1650,6 +1673,9 @@ template<typename T, typename U,
 struct __is_byte_constructible {
 	constexpr static bool is_copy = sizeof(T) == sizeof(U);
 	constexpr static bool is_fill = true;
+	constexpr static T get(const U& val) {
+		return static_cast<T>(val);
+	}
 };
 
 template<typename T, bool = 
@@ -1658,6 +1684,9 @@ template<typename T, bool =
 struct __is_byte_copy_constructible_helper {
 	constexpr static bool is_copy = true;
 	constexpr static bool is_fill = !std::is_empty_v<T>;
+	constexpr static const T& get(const T& val){
+		return val;
+	}
 };
 
 template<typename T>
@@ -1672,6 +1701,9 @@ template<typename T, bool =
 struct __is_byte_move_constructible_helper {
 	constexpr static bool is_copy = true;
 	constexpr static bool is_fill = !std::is_empty_v<T>;
+	constexpr static const T& get(const T& val) {
+		return val;
+	}
 };
 
 template<typename T>
@@ -7143,7 +7175,9 @@ static void __memset_helper(T* s, const U& val, size_t n) {
 		return;
 	}
 
-	auto __bytes_num = _Get_bytes_num(static_cast<T>(val));
+	const auto& _Val = TEST<T, const U&>::get(val);
+
+	auto __bytes_num = _Get_bytes_num<sizeof(T)>(_Val);
 
 #if defined(__HAS_FAST_MEMSET)
 	constexpr auto __max_bytes_num = _Get_max_bytes_num<sizeof(T)>();
@@ -7161,7 +7195,7 @@ static void __memset_helper(T* s, const U& val, size_t n) {
 
 	using value_type = uint_t<__max_bytes_num * 8>;
 	auto __s = reinterpret_cast<value_type*>(s);
-	auto __val = *reinterpret_cast<const value_type*>(&val);
+	auto __val = *reinterpret_cast<const value_type*>(&_Val);
 	static_assert(std::is_same_v<decltype(__val), value_type>, "type mismatch");
 	
 #if defined(__HAS_FAST_MEMSET)
