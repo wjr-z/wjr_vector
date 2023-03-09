62545bce69853bec60461f5947a5b9a70a154413
wjrwjr <1966336874@qq.com>
2023-03-09 21:46:23 +0800

    upd

--- vector.h
+++ vector.h
@@ -8139,210 +8139,6 @@ _WJR_ALGO_END
 	simd_t::storeu(reinterpret_cast<sint*>((ptr) + width * 3), q);
 #endif // __WJR_MEMSET64byte
 
-//#if defined(__TEST)
-#pragma once
-#ifndef __WJR_RANDOM_H
-#define __WJR_RANDOM_H
-
-#include <random>
-
-
-
-_WJR_BEGIN
-
-template<typename Engine>
-struct basic_random_static_wrapper {
-	static Engine m_engine;
-	static Engine& engine() {
-		return m_engine;
-	}
-};
-
-template<typename Engine>
-Engine basic_random_static_wrapper<Engine>::m_engine(std::random_device{}());
-
-template<typename Engine>
-struct basic_random_thread_local_wrapper {
-	static Engine& engine() {
-		thread_local Engine m_engine(std::random_device{}());
-		return m_engine;
-	}
-};
-
-template<
-	typename Engine,
-	template<typename> typename EngineWrapper,
-	template<typename> typename IntegerDist = std::uniform_int_distribution,
-	template<typename> typename RealDist = std::uniform_real_distribution,
-	typename BoolDist = std::bernoulli_distribution
->
-class basic_random : public EngineWrapper<Engine> {
-
-	template<typename T>
-	using _Is_container = std::conjunction<
-		std::has_global_function_begin<T>,
-		std::has_global_function_end<T>
-	>;
-
-	template<typename T>
-	using _Has_size = std::has_global_function_size<T>;
-
-public:
-	using _Mybase = EngineWrapper<Engine>;
-
-	using engine_type = Engine;
-
-	template<typename T>
-	using integer_dist_t = IntegerDist<T>;
-
-	template<typename T>
-	using real_dist_t = RealDist<T>;
-
-	using bool_dist_t = BoolDist;
-
-	using common = std::common_type<>;
-
-	using _Mybase::engine;
-
-	basic_random() = delete;
-
-	constexpr static typename engine_type::result_type min() {
-		return engine_type::min();
-	}
-
-	constexpr static typename engine_type::result_type max() {
-		return engine_type::max();
-	}
-
-	static void discard(size_t n) {
-		engine().discard(n);
-	}
-
-	static void reseed() {
-		engine().seed(std::random_device{}());
-	}
-
-	static void seed(typename engine_type::result_type seed = engine_type::default_seed) {
-		engine().seed(seed);
-	}
-
-	template<typename Seq>
-	static void seed(Seq& seq) {
-		engine().seed(seq);
-	}
-
-	static typename engine_type::result_type get() {
-		return engine()();
-	}
-
-	template<typename T, std::enable_if_t<std::conjunction_v<
-		std::is_arithmetic<T>, std::negation<std::is_same<T, bool>>>, int> = 0>
-	static T get(
-		T __min = std::numeric_limits<T>::min(),
-		T __max = std::numeric_limits<T>::max()) {
-		if constexpr (std::is_integral_v<T>) {
-			if constexpr (sizeof(T) < sizeof(short)) {
-				using short_t = std::conditional_t<std::is_signed<T>::value,
-					short, unsigned short>;
-				return static_cast<T>(integer_dist_t<short_t>{__min, __max}(engine()));
-			}
-			else {
-				using type = int_or_uint_t<sizeof(T) * 8, std::is_signed_v<T>>;
-				return integer_dist_t<type>{__min, __max}(engine());
-			}
-		}
-		else {
-			return real_dist_t<T>{__min, __max}(engine());
-		}
-	}
-
-	template<typename X, typename T, typename U, typename R = std::common_type_t<T, U>,
-		std::enable_if_t<std::conjunction_v<
-		std::is_same<X, common>,
-		std::is_arithmetic<T>,
-		std::is_arithmetic<U>>, int> = 0>
-		static R get(
-			T __min = std::numeric_limits<T>::min(),
-			U __max = std::numeric_limits<U>::max()) {
-		return get<R>(static_cast<R>(__min), static_cast<R>(__max));
-	}
-
-	template<typename T, std::enable_if_t<std::is_same_v<T, bool>, int> = 0>
-	static T get(double p = 0.5) {
-		return bool_dist_t{ p }(engine());
-	}
-
-	template<typename Dist, typename...Args>
-	static typename Dist::result_type get(Args&&...args) {
-		return Dist{ std::forward<Args>(args)... }(engine());
-	}
-
-	template<typename Dist>
-	static typename Dist::result_type get(Dist& dist) {
-		return dist(engine());
-	}
-
-	template<typename _Container, std::enable_if_t<std::conjunction_v<
-		std::has_global_function_begin<_Container>,
-		std::has_global_function_end<_Container>>, int> = 0>
-	static decltype(auto) get(_Container& container) {
-		auto first = std::begin(container);
-		auto last = std::end(container);
-		auto n = wjr::size(container);
-		if (n == 0)return last;
-		using diff_t = wjr::iter_diff_t<decltype(first)>;
-		return std::next(first, get<diff_t>(0, n - 1));
-	}
-
-	template<typename iter, std::enable_if_t<wjr::is_iterator_v<iter>, int> = 0>
-	static iter get(iter first, iter last) {
-		return get(wjr::make_iter_wrapper(first, last));
-	}
-
-	template<typename T>
-	static T get(std::initializer_list<T> il) {
-		return *get(std::begin(il), std::end(il));
-	}
-
-	template<typename _Ty, size_t _Size>
-	static _Ty get(_Ty(&arr)[_Size]) {
-		return *get(std::begin(arr), std::end(arr));
-	}
-
-	template<typename _Container>
-	static void shuffle(_Container& container) {
-		std::shuffle(std::begin(container), std::end(container), engine());
-	}
-
-	template<typename iter>
-	static void shuffle(iter first, iter last) {
-		shuffle(wjr::make_iter_wrapper(first, last));
-	}
-
-};
-
-template<typename Engine,
-	template<typename>typename IntegerDist = std::uniform_int_distribution,
-	template<typename>typename RealDist = std::uniform_real_distribution,
-	typename BoolDist = std::bernoulli_distribution>
-using basic_random_static = basic_random<Engine, basic_random_static_wrapper, IntegerDist, RealDist, BoolDist>;
-
-template<typename Engine,
-	template<typename>typename IntegerDist = std::uniform_int_distribution,
-	template<typename>typename RealDist = std::uniform_real_distribution,
-	typename BoolDist = std::bernoulli_distribution>
-using basic_random_thread_local = basic_random<Engine, basic_random_thread_local_wrapper, IntegerDist, RealDist, BoolDist>;
-
-using random_static = basic_random_static<std::mt19937>;
-using random_thread_local = basic_random_thread_local<std::mt19937>;
-
-using Random = random_static;
-
-_WJR_END
-
-#endif // __WJR_RANDOM_H
-//#endif
-
 _WJR_ALGO_BEGIN
 
 template<typename T>
@@ -8370,13 +8166,13 @@ void __memset(T* s, T val, size_t n) {
 	}
 
 	constexpr size_t __constant_threshold = 2048 / _Mysize;
-	constexpr size_t __basic_align_threshold = 1024 / _Mysize;
+	// threshold for enhanced rep
+	// enhanced rep will be faster when 64-byte memory is aligned
+	// but it will be slower when 64-byte memory is not aligned
+	// so we use a threshold to determine whether to use enhanced rep
+	constexpr size_t __rep_align64byte_threshold = 1024 / _Mysize;
 
-#if defined(__TEST)
-	const bool __use_rep = wjr::Random::get<bool>();
-#else
 	const bool __use_rep = wjr::is_enhanced_rep();
-#endif
 
 #if defined(_WJR_NON_TEMPORARY)
 	const size_t __nt_threshold = (wjr::max_cache_size() + wjr::max_cache_size() / 8) / _Mysize;
@@ -8391,68 +8187,65 @@ void __memset(T* s, T val, size_t n) {
 
 	if (n >= 16 / _Mysize) {
 		auto qx = simd::sse::set1(val, T());
-		if (n >= 128 / _Mysize) {
-			// [128, ...)
-			auto q = broadcast<sint, __m128i>(qx);
 
-			if (is_likely(reinterpret_cast<uintptr_t>(s) % _Mysize == 0)) {
+		if (n <= 32 / _Mysize) {
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s), qx);
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (16 / _Mysize)), qx);
+			return;
+		}
 
-				if (n < __basic_align_threshold) {
+#if WJR_AVX2
+
+		if (n < 128 / _Mysize) {
+			auto qy = wjr::broadcast<__m256i, __m128i>(qx);
+			auto delta = (n & (64 / _Mysize)) >> 1;
+			simd::avx::storeu(reinterpret_cast<__m256i*>(s), qy);
+			simd::avx::storeu(reinterpret_cast<__m256i*>(s + delta), qy);
+			simd::avx::storeu(reinterpret_cast<__m256i*>(s + n - (32 / _Mysize) - delta), qy);
+			simd::avx::storeu(reinterpret_cast<__m256i*>(s + n - (32 / _Mysize)), qy);
+			return;
+		}
+
+#else
+
+		if (n < 128 / _Mysize) {
+			auto delta = (n & (64 / _Mysize)) >> 1;
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s), qx);
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s + (16 / _Mysize)), qx);
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s + delta), qx);
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s + delta + (16 / _Mysize)), qx);
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (32 / _Mysize) - delta), qx);
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (16 / _Mysize) - delta), qx);
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (32 / _Mysize)), qx);
+			simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (16 / _Mysize)), qx);
+			return;
+		}
+
+#endif // WJR_AVX2
+
+		// [128, ...)
+		auto q = broadcast<sint, __m128i>(qx);
+
+		if (is_likely(reinterpret_cast<uintptr_t>(s) % _Mysize == 0)) {
 
 #if defined(_WJR_FAST_REP)
-					if (__use_rep) {
-						auto u64v = broadcast<uint64_t, T>(val);
-						if (is_constant_p(reinterpret_cast<uintptr_t>(s) % 8) &&
-							reinterpret_cast<uintptr_t>(s) % 8 == 0) {
-						}
-						else {
-							*reinterpret_cast<uint64_t*>(s) = u64v;
-							auto __align_s = 8 - reinterpret_cast<uintptr_t>(s) % 8;
-							s += __align_s / _Mysize;
-							n -= __align_s / _Mysize;
-						}
-						*reinterpret_cast<uint64_t*>(s + n - 8 / _Mysize) = u64v;
-						n &= -(8 / _Mysize);
-						n /= (8 / _Mysize);
-						wjr::masm::rep_stosq(reinterpret_cast<unsigned long long*>(s), u64v, n);
+			if (__use_rep) {
+
+				if (n < __rep_align64byte_threshold) {
+					auto u64v = broadcast<uint64_t, T>(val);
+					if (is_constant_p(reinterpret_cast<uintptr_t>(s) % 8) &&
+						reinterpret_cast<uintptr_t>(s) % 8 == 0) {
 					}
 					else {
-#endif // _WJR_FAST_REP
-
-						if (is_constant_p(reinterpret_cast<uintptr_t>(s) % bound) &&
-							reinterpret_cast<uintptr_t>(s) % bound == 0) {
-							// do nothing
-						}
-						else {
-							simd_t::storeu(reinterpret_cast<sint*>(s), q);
-							auto __align_s = bound - reinterpret_cast<uintptr_t>(s) % bound;
-							s += __align_s / _Mysize;
-							n -= __align_s / _Mysize;
-							if (n < width * 4) goto WJR_MACRO_LABEL(aft_align);
-						}
-
-						do {
-							simd_t::store(reinterpret_cast<sint*>(s), q);
-							simd_t::store(reinterpret_cast<sint*>(s + width), q);
-							simd_t::store(reinterpret_cast<sint*>(s + width * 2), q);
-							simd_t::store(reinterpret_cast<sint*>(s + width * 3), q);
-							s += width * 4;
-							n -= width * 4;
-						} while (n >= width * 4);
-
-						WJR_MACRO_LABEL(aft_align) :
-
-							s += n;
-						auto ptr = reinterpret_cast<T*>(
-							(reinterpret_cast<uintptr_t>(s - width * 3)) & (~(bound - 1)));
-						simd_t::store(reinterpret_cast<sint*>(ptr), q);
-						simd_t::store(reinterpret_cast<sint*>(ptr + width), q);
-						simd_t::store(reinterpret_cast<sint*>(ptr + width * 2), q);
-						simd_t::storeu(reinterpret_cast<sint*>(s - width), q);
-#if defined(_WJR_FAST_REP)
+						*reinterpret_cast<uint64_t*>(s) = u64v;
+						auto __align_s = 8 - reinterpret_cast<uintptr_t>(s) % 8;
+						s += __align_s / _Mysize;
+						n -= __align_s / _Mysize;
 					}
-#endif // _WJR_FAST_REP
-
+					*reinterpret_cast<uint64_t*>(s + n - 8 / _Mysize) = u64v;
+					n &= -(8 / _Mysize);
+					n /= (8 / _Mysize);
+					wjr::masm::rep_stosq(reinterpret_cast<unsigned long long*>(s), u64v, n);
 					return;
 				}
 
@@ -8463,10 +8256,7 @@ void __memset(T* s, T val, size_t n) {
 					// do nothing
 				}
 				else {
-					simd_t::storeu(reinterpret_cast<sint*>((s)), q);
-					simd_t::storeu(reinterpret_cast<sint*>((s)+width), q);
-					simd_t::storeu(reinterpret_cast<sint*>((s)+width * 2), q);
-					simd_t::storeu(reinterpret_cast<sint*>((s)+width * 3), q);
+					__WJR_ALIGN64byte(s);
 					auto __align_s = 64 - reinterpret_cast<uintptr_t>(s) % 64;
 					s += __align_s / _Mysize;
 					n -= __align_s / _Mysize;
@@ -8476,112 +8266,109 @@ void __memset(T* s, T val, size_t n) {
 				if (n < __nt_threshold) {
 #endif // _WJR_NON_TEMPORARY
 
-#if defined(_WJR_FAST_REP)
-					if (__use_rep) {
-						// store last 64 byte
-						__WJR_ALIGN64byte(s + n - (64 / _Mysize));
+					__WJR_ALIGN64byte(s + n - (64 / _Mysize));
 
-						n = (n - 1) & (-(64 / _Mysize));
-						n /= (8 / _Mysize);
-						auto u64v = broadcast<uint64_t, T>(val);
-						wjr::masm::rep_stosq(reinterpret_cast<unsigned long long*>(s), u64v, n);
-						return;
-					}
-					else {
-#endif // _WJR_FAST_REP
-
-						do {
-							simd_t::store(reinterpret_cast<sint*>(s), q);
-							simd_t::store(reinterpret_cast<sint*>(s + width), q);
-							simd_t::store(reinterpret_cast<sint*>(s + width * 2), q);
-							simd_t::store(reinterpret_cast<sint*>(s + width * 3), q);
-							s += width * 4;
-							n -= width * 4;
-						} while (n >= width * 4);
-#if defined(_WJR_FAST_REP)
-					}
-#endif // _WJR_FAST_REP
+					n = (n - 1) & (-(64 / _Mysize));
+					n /= (8 / _Mysize);
+					auto u64v = broadcast<uint64_t, T>(val);
+					wjr::masm::rep_stosq(reinterpret_cast<unsigned long long*>(s), u64v, n);
+					return;
 
 #if defined(_WJR_NON_TEMPORARY)
 				}
-				else {
+#endif // _WJR_NON_TEMPORARY
+
+			}
+			else {
+#endif // _WJR_FAST_REP
+
+#if defined(_WJR_NON_TEMPORARY)
+				if (n < __nt_threshold) {
+#endif // _WJR_NON_TEMPORARY
+
+					if (is_constant_p(reinterpret_cast<uintptr_t>(s) % bound) &&
+						reinterpret_cast<uintptr_t>(s) % bound == 0) {
+						// do nothing
+					}
+					else {
+						simd_t::storeu(reinterpret_cast<sint*>(s), q);
+						auto __align_s = bound - reinterpret_cast<uintptr_t>(s) % bound;
+						s += __align_s / _Mysize;
+						n -= __align_s / _Mysize;
+						if (n < width * 4) goto WJR_MACRO_LABEL(last_store);
+					}
+
 					do {
-						simd_t::stream(reinterpret_cast<sint*>(s), q);
-						simd_t::stream(reinterpret_cast<sint*>(s + width), q);
-						simd_t::stream(reinterpret_cast<sint*>(s + width * 2), q);
-						simd_t::stream(reinterpret_cast<sint*>(s + width * 3), q);
+						simd_t::store(reinterpret_cast<sint*>(s), q);
+						simd_t::store(reinterpret_cast<sint*>(s + width), q);
+						simd_t::store(reinterpret_cast<sint*>(s + width * 2), q);
+						simd_t::store(reinterpret_cast<sint*>(s + width * 3), q);
 						s += width * 4;
 						n -= width * 4;
 					} while (n >= width * 4);
-					simd::sse::sfence();
+
+					goto WJR_MACRO_LABEL(last_store);
+
+#if defined(_WJR_NON_TEMPORARY)
 				}
 #endif // _WJR_NON_TEMPORARY
 
-				// store last (width * 4) byte
+#if defined(_WJR_FAST_REP)
+			}
+#endif // _WJR_FAST_REP
 
-				s += n;
-				auto ptr = reinterpret_cast<T*>(
-					(reinterpret_cast<uintptr_t>(s - width * 3)) & (~(bound - 1)));
-				simd_t::store(reinterpret_cast<sint*>(ptr), q);
-				simd_t::store(reinterpret_cast<sint*>(ptr + width), q);
-				simd_t::store(reinterpret_cast<sint*>(ptr + width * 2), q);
-				simd_t::storeu(reinterpret_cast<sint*>(s - width), q);
+			// align first 64 byte
 
-				return;
+			if (is_constant_p(reinterpret_cast<uintptr_t>(s) % 64) &&
+				reinterpret_cast<uintptr_t>(s) % 64 == 0) {
+				// do nothing
+			}
+			else {
+				__WJR_ALIGN64byte(s);
+				auto __align_s = 64 - reinterpret_cast<uintptr_t>(s) % 64;
+				s += __align_s / _Mysize;
+				n -= __align_s / _Mysize;
 			}
 
-			// non-aligned algorithm
 			do {
-				simd_t::storeu(reinterpret_cast<sint*>(s), q);
-				simd_t::storeu(reinterpret_cast<sint*>(s + width), q);
-				simd_t::storeu(reinterpret_cast<sint*>(s + width * 2), q);
-				simd_t::storeu(reinterpret_cast<sint*>(s + width * 3), q);
+				simd_t::stream(reinterpret_cast<sint*>(s), q);
+				simd_t::stream(reinterpret_cast<sint*>(s + width), q);
+				simd_t::stream(reinterpret_cast<sint*>(s + width * 2), q);
+				simd_t::stream(reinterpret_cast<sint*>(s + width * 3), q);
 				s += width * 4;
 				n -= width * 4;
 			} while (n >= width * 4);
-			s += n;
-			simd_t::storeu(reinterpret_cast<sint*>(s - width * 4), q);
-			simd_t::storeu(reinterpret_cast<sint*>(s - width * 3), q);
-			simd_t::storeu(reinterpret_cast<sint*>(s - width * 2), q);
+			simd::sse::sfence();
+
+			// store last (width * 4) byte
+
+			WJR_MACRO_LABEL(last_store) :
+
+				s += n;
+			auto ptr = reinterpret_cast<T*>(
+				(reinterpret_cast<uintptr_t>(s - width * 3)) & (~(bound - 1)));
+			simd_t::store(reinterpret_cast<sint*>(ptr), q);
+			simd_t::store(reinterpret_cast<sint*>(ptr + width), q);
+			simd_t::store(reinterpret_cast<sint*>(ptr + width * 2), q);
 			simd_t::storeu(reinterpret_cast<sint*>(s - width), q);
+
 			return;
 		}
 
-#if WJR_AVX2
-		// [16, 128)
-		static_assert(width * 4 == 128 / _Mysize, "width * 4 == 128 / _Mysize");
-		if (n > 32 / _Mysize) {
-			// (32, 128)
-			auto qy = wjr::broadcast<__m256i, __m128i>(qx);
-			auto delta = (n & (64 / _Mysize)) >> 1;
-			simd::avx::storeu(reinterpret_cast<__m256i*>(s), qy);
-			simd::avx::storeu(reinterpret_cast<__m256i*>(s + delta), qy);
-			simd::avx::storeu(reinterpret_cast<__m256i*>(s + n - (32 / _Mysize) - delta), qy);
-			simd::avx::storeu(reinterpret_cast<__m256i*>(s + n - (32 / _Mysize)), qy);
-			return;
-		}
-		// [16, 32]
-		simd::sse::storeu(reinterpret_cast<__m128i*>(s), qx);
-		simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (16 / _Mysize)), qx);
-#else
-		// [16, 128)
-		if (n >= 64) {
-			simd::sse::storeu(reinterpret_cast<__m128i*>(s), qx);
-			simd::sse::storeu(reinterpret_cast<__m128i*>(s + (16 / _Mysize)), qx);
-			simd::sse::storeu(reinterpret_cast<__m128i*>(s + (32 / _Mysize)), qx);
-			simd::sse::storeu(reinterpret_cast<__m128i*>(s + (48 / _Mysize)), qx);
-			simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (64 / _Mysize)), qx);
-			simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (48 / _Mysize)), qx);
-			simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (32 / _Mysize)), qx);
-			simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (16 / _Mysize)), qx);
-			return;
-		}
-		auto delta = (n & (32 / _Mysize)) >> 1;
-		simd::sse::storeu(reinterpret_cast<__m128i*>(s), qx);
-		simd::sse::storeu(reinterpret_cast<__m128i*>(s + delta), qx);
-		simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (16 / _Mysize) - delta), qx);
-		simd::sse::storeu(reinterpret_cast<__m128i*>(s + n - (16 / _Mysize)), qx);
-#endif // WJR_AVX2
+		// non-aligned algorithm
+		do {
+			simd_t::storeu(reinterpret_cast<sint*>(s), q);
+			simd_t::storeu(reinterpret_cast<sint*>(s + width), q);
+			simd_t::storeu(reinterpret_cast<sint*>(s + width * 2), q);
+			simd_t::storeu(reinterpret_cast<sint*>(s + width * 3), q);
+			s += width * 4;
+			n -= width * 4;
+		} while (n >= width * 4);
+		s += n;
+		simd_t::storeu(reinterpret_cast<sint*>(s - width * 4), q);
+		simd_t::storeu(reinterpret_cast<sint*>(s - width * 3), q);
+		simd_t::storeu(reinterpret_cast<sint*>(s - width * 2), q);
+		simd_t::storeu(reinterpret_cast<sint*>(s - width), q);
 		return;
 	}
 
