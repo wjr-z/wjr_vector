c04e662daae4e721ba2f1742c64e949d325db160
wjrwjr <1966336874@qq.com>
2023-03-12 11:17:42 +0800

    upd

--- vector.h
+++ vector.h
@@ -7375,7 +7375,6 @@ _WJR_ALGO_BEGIN
 
 template<typename T, typename _Pred>
 const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
-	using namespace wjr::literals;
 	constexpr size_t _Mysize = sizeof(T);
 
 #if WJR_AVX2
@@ -7384,6 +7383,7 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 	using simd_t = simd::sse;
 #endif // WJR_AVX2
 	using sint = typename simd_t::int_type;
+	
 	constexpr uintptr_t width = simd_t::width() / (8 * _Mysize);
 	constexpr uintptr_t bound = width * _Mysize;
 
@@ -7397,38 +7397,41 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 			auto y = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1));
 
 			__WJR_MEMMIS_ONE(simd::sse, s0);
-
+			
 			// solve last 16 bytes
 
-			s0 += n;
+			// Avoid temporary registers
+			s0 += n - 16 / _Mysize;
+			s1 += n - 16 / _Mysize;
 
-			x = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - 16 / _Mysize));
-			y = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 + n - 16 / _Mysize));
+			x = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0));
+			y = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1));
 
-			__WJR_MEMMIS_ONE(simd::sse, s0 - 16 / _Mysize);
-
-			return s0;
+			__WJR_MEMMIS_ONE(simd::sse, s0);
+			
+			return s0 + 16 / _Mysize;
 		}
 
 		// solve first min(n, 128 / _Mysize) bytes
 		// no branch algorithm
-
+		
 		{
 			const auto m = n <= 128 / _Mysize ? n : 128 / _Mysize;
 			const auto delta = n < (64 / _Mysize) ? 0 : (32 / _Mysize);
+			const auto negdelta = m - 32 / _Mysize;
 
 #if WJR_AVX2
 			auto x0 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0));
 			auto x1 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 + delta));
-			auto x2 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 + m - (32 / _Mysize) - delta));
-			auto x3 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 + m - (32 / _Mysize)));
+			auto x2 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 + negdelta - delta));
+			auto x3 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 + negdelta));
 
 			auto y0 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1));
 			auto y1 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 + delta));
-			auto y2 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 + m - (32 / _Mysize) - delta));
-			auto y3 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 + m - (32 / _Mysize)));
+			auto y2 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 + negdelta - delta));
+			auto y3 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 + negdelta));
 
-			__WJR_MEMMIS_FOUR(simd::avx, s0, s0 + delta, s0 + m - (32 / _Mysize) - delta, s0 + m - (32 / _Mysize));
+			__WJR_MEMMIS_FOUR(simd::avx, s0, s0 + delta, s0 + negdelta - delta, s0 + negdelta);
 #else
 			{
 				auto x0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0));
@@ -7445,21 +7448,21 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 			}
 
 			{
-				auto x0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 + m - (32 / _Mysize) - delta));
-				auto x1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 + m - (16 / _Mysize) - delta));
-				auto x2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 + m - (32 / _Mysize)));
-				auto x3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 + m - (16 / _Mysize)));
+				auto x0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 + negdelta - delta));
+				auto x1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 + negdelta - delta + 16 / _Mysize));
+				auto x2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 + negdelta));
+				auto x3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 + negdelta + 16 / _Mysize));
 
-				auto y0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 + m - (32 / _Mysize) - delta));
-				auto y1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 + m - (16 / _Mysize) - delta));
-				auto y2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 + m - (32 / _Mysize)));
-				auto y3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 + m - (16 / _Mysize)));
+				auto y0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 + negdelta - delta));
+				auto y1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 + negdelta - delta + 16 / _Mysize));
+				auto y2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 + negdelta));
+				auto y3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 + negdelta + 16 / _Mysize));
 
 				__WJR_MEMMIS_FOUR(simd::sse,
-					s0 + m - (32 / _Mysize) - delta,
-					s0 + m - (16 / _Mysize) - delta,
-					s0 + m - (32 / _Mysize),
-					s0 + m - (16 / _Mysize));
+					s0 + negdelta - delta,
+					s0 + negdelta - delta + 16 / _Mysize,
+					s0 + negdelta,
+					s0 + negdelta + 16 / _Mysize);
 			}
 #endif // WJR_AVX2
 		}
@@ -7475,8 +7478,8 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 		if (n <= width * 4) {
 
 			WJR_MACRO_LABEL(unaligned_last_4vec) :
-
-				s0 += n;
+			
+			s0 += n;
 			s1 += n;
 
 			auto x0 = simd_t::loadu(reinterpret_cast<const sint*>(s0 - width * 4));
@@ -7503,7 +7506,7 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 
 			if (off0 != off1) {
 				// only align first pointer
-
+				
 				if (is_constant_p(off0) && off0 == 0) {
 					// do nothing
 				}
@@ -7517,7 +7520,7 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 					s0 += __align_s / _Mysize;
 					s1 += __align_s / _Mysize;
 					n -= __align_s / _Mysize;
-
+					
 					if (is_unlikely(n < width * 4)) {
 						goto WJR_MACRO_LABEL(last_solve_align_0);
 					}
@@ -7545,7 +7548,7 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 
 					WJR_MACRO_LABEL(last_solve_align_0) :
 
-						s0 += n;
+					s0 += n;
 					s1 += n;
 
 					const auto ptr0 = reinterpret_cast<T*>(
@@ -7583,7 +7586,7 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 				s0 += __align_s / _Mysize;
 				s1 += __align_s / _Mysize;
 				n -= __align_s / _Mysize;
-
+				
 				if (is_unlikely(n < width * 4)) {
 					goto WJR_MACRO_LABEL(last_solve_align_0_1);
 				}
@@ -7611,7 +7614,7 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 
 				WJR_MACRO_LABEL(last_solve_align_0_1) :
 
-					s0 += n;
+				s0 += n;
 				s1 += n;
 
 				auto ptr0 = reinterpret_cast<T*>(
@@ -7680,7 +7683,7 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 
 				WJR_MACRO_LABEL(last_solve_align_1) :
 
-					s0 += n;
+				s0 += n;
 				s1 += n;
 
 				const auto ptr1 = reinterpret_cast<T*>(
@@ -7703,7 +7706,7 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 
 			return s0;
 		}
-
+		
 		// unaligned algorithm
 		do {
 			auto x0 = simd_t::loadu(reinterpret_cast<const sint*>(s0));
@@ -7723,10 +7726,10 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 			n -= width * 4;
 		} while (n >= width * 4);
 
-		if (n != 0) {
+		if (is_likely(n != 0)) {
 			goto WJR_MACRO_LABEL(unaligned_last_4vec);
 		}
-
+		
 		return s0;
 	}
 
@@ -7790,11 +7793,13 @@ const T* __memmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 		// n = [1, 4)
 		if (!pred(s0[0], s1[0])) return s0;
 		if (n == 1) return s0 + 1;
-		if (!pred(s0[1], s1[1])) return s0 + 1;
-		if (n == 2) return s0 + 2;
-		return pred(s0[2], s1[2]) ? s0 + 3 : s0 + 2;
+		const bool f = pred(s0[n - 2], s1[n - 2]);
+		const bool g = pred(s0[n - 1], s1[n - 1]);
+		const size_t i1 = g ? n : n - 1;
+		const size_t i2 = f ? i1 : n - 2;
+		return s0 + i2;
 	}
-
+	
 }
 
 _WJR_ALGO_END
@@ -7809,36 +7814,40 @@ _WJR_ALGO_END
 
 #if defined(_WJR_FAST_MEMMIS)
 
-#define __WJR_MEMRMIS_ONE(st, _s)														\
-	auto r = st::cmp(x, y, pred, T());	                                                \
-	st::mask_type z = st::movemask_epi8(r);		                                        \
-	if(z != st::mask()) {	                                                            \
-		return (_s) - wjr::countl_one(z) / _Mysize;		                                \
+#define __WJR_MEMRMIS_ONE(st, _s)														    \
+	{	                                                                                    \
+		auto r = st::cmp(x, y, pred, T());	                                                \
+		st::mask_type z = st::movemask_epi8(r);		                                        \
+		if(z != st::mask()) {	                                                            \
+			return (_s) - wjr::countl_one(z) / _Mysize;		                                \
+		}	                                                                                \
 	}
 
-#define __WJR_MEMRMIS_FOUR(st, _s0, _s1, _s2, _s3)										\
-	auto r0 = st::cmp(x0, y0, pred, T());	                                            \
-	auto r1 = st::cmp(x1, y1, pred, T());	                                            \
-	auto r2 = st::cmp(x2, y2, pred, T());	                                            \
-	auto r3 = st::cmp(x3, y3, pred, T());	                                            \
-		                                                                                \
-	r0 = st::And(st::And(r0, r1), st::And(r2, r3));	                                    \
-	st::mask_type z = st::movemask_epi8(r0);		                                    \
-	if(z != st::mask()) {	                                                            \
-		st::mask_type tmp = st::movemask_epi8(r3);	                                    \
-		if(tmp != st::mask()){	                                                        \
-			return (_s3) - wjr::countl_one(tmp) / _Mysize;	                            \
-		}	                                                                            \
-		tmp = st::movemask_epi8(r2);	                                                \
-		if(tmp != st::mask()){	                                                        \
-			return (_s2) - wjr::countl_one(tmp) / _Mysize;	                            \
-		}	                                                                            \
-		tmp = st::movemask_epi8(r1);	                                                \
-		if(tmp != st::mask()){	                                                        \
-			return (_s1) - wjr::countl_one(tmp) / _Mysize;	                            \
-		}	                                                                            \
-		tmp = z;	                                                                    \
-		return (_s0) - wjr::countl_one(tmp) / _Mysize;	                                \
+#define __WJR_MEMRMIS_FOUR(st, _s0, _s1, _s2, _s3)										    \
+	{	                                                                                    \
+		auto r0 = st::cmp(x0, y0, pred, T());	                                            \
+		auto r1 = st::cmp(x1, y1, pred, T());	                                            \
+		auto r2 = st::cmp(x2, y2, pred, T());	                                            \
+		auto r3 = st::cmp(x3, y3, pred, T());	                                            \
+																							\
+		r0 = st::And(st::And(r0, r1), st::And(r2, r3));	                                    \
+		st::mask_type z = st::movemask_epi8(r0);		                                    \
+		if(z != st::mask()) {	                                                            \
+			st::mask_type tmp = st::movemask_epi8(r3);	                                    \
+			if(tmp != st::mask()){	                                                        \
+				return (_s3) - wjr::countl_one(tmp) / _Mysize;	                            \
+			}	                                                                            \
+			tmp = st::movemask_epi8(r2);	                                                \
+			if(tmp != st::mask()){	                                                        \
+				return (_s2) - wjr::countl_one(tmp) / _Mysize;	                            \
+			}	                                                                            \
+			tmp = st::movemask_epi8(r1);	                                                \
+			if(tmp != st::mask()){	                                                        \
+				return (_s1) - wjr::countl_one(tmp) / _Mysize;	                            \
+			}	                                                                            \
+			tmp = z;	                                                                    \
+			return (_s0) - wjr::countl_one(tmp) / _Mysize;	                                \
+		}	                                                                                \
 	}
 
 _WJR_ALGO_BEGIN
@@ -7864,44 +7873,117 @@ const T* __memrmis(const T* s0, const T* s1, size_t n, _Pred pred) {
 
 	if (n >= 16 / _Mysize) {
 
-		if (n < 64 / _Mysize) {
-			auto delta = (n & (32 / _Mysize)) >> 1;
+		if (n <= 32 / _Mysize) {
+			// solve first 16 bytes
+			auto x = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - (16 / _Mysize)));
+			auto y = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - (16 / _Mysize)));
 
-			auto x0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - n));
-			auto x1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - n + delta));
-			auto x2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - 16 / _Mysize - delta));
-			auto x3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - 16 / _Mysize));
+			__WJR_MEMRMIS_ONE(simd::sse, s0);
 
-			auto y0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - n));
-			auto y1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - n + delta));
-			auto y2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - 16 / _Mysize - delta));
-			auto y3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - 16 / _Mysize));
+			// solve last 16 bytes
 
-			__WJR_MEMRMIS_FOUR(simd::sse, s0 - n + 16 / _Mysize, s0 - n + delta + 16 / _Mysize, s0 - delta, s0);
+			s0 -= n;
+			s1 -= n;
 
-			return s0 - n;
+			x = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0));
+			y = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1));
+
+			__WJR_MEMRMIS_ONE(simd::sse, s0 + (16 / _Mysize));
+
+			return s0;
 		}
+
+		// solve first min(n, 128 / _Mysize) bytes
+		// no branch algorithm
+
+		{
+			const auto m = n <= 128 / _Mysize ? n : 128 / _Mysize;
+			const auto delta = n < (64 / _Mysize) ? 0 : (32 / _Mysize);
 
 #if WJR_AVX2
-		static_assert(width * 4 == 128 / _Mysize, "width * 4 == 128 / _Mysize");
-		if (n < 128 / _Mysize) {
-			auto x0 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 - n));
-			auto x1 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 - n + 32 / _Mysize));
-			auto x2 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 - 64 / _Mysize));
-			auto x3 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 - 32 / _Mysize));
+			auto x0 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 - m));
+			auto x1 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 - m + delta));
+			auto x2 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 - (32 / _Mysize) - delta));
+			auto x3 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s0 - (32 / _Mysize)));
 
-			auto y0 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 - n));
-			auto y1 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 - n + 32 / _Mysize));
-			auto y2 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 - 64 / _Mysize));
-			auto y3 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 - 32 / _Mysize));
+			auto y0 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 - m));
+			auto y1 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 - m + delta));
+			auto y2 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 - (32 / _Mysize) - delta));
+			auto y3 = simd::avx::loadu(reinterpret_cast<const __m256i*>(s1 - (32 / _Mysize)));
 
-			__WJR_MEMRMIS_FOUR(simd::avx, s0 - n + 32 / _Mysize, s0 - n + 64 / _Mysize, s0 - 32 / _Mysize, s0);
+			__WJR_MEMRMIS_FOUR(simd::avx, 
+				s0 - m + 32 / _Mysize, 
+				s0 - m + delta + 32 / _Mysize, 
+				s0 - delta, 
+				s0);
+#else
+			{
+				auto x0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - (32 / _Mysize) - delta));
+				auto x1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - (16 / _Mysize) - delta));
+				auto x2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - (32 / _Mysize)));
+				auto x3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - (16 / _Mysize)));
 
+				auto y0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - (32 / _Mysize) - delta));
+				auto y1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - (16 / _Mysize) - delta));
+				auto y2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - (32 / _Mysize)));
+				auto y3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - (16 / _Mysize)));
+
+				__WJR_MEMRMIS_FOUR(simd::sse,
+					s0 - (16 / _Mysize) - delta,
+					s0 - delta,
+					s0 - (16 / _Mysize),
+					s0);
+			}
+			
+			{
+				auto x0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - m));
+				auto x1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - m + 16 / _Mysize));
+				auto x2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - m + delta));
+				auto x3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s0 - m + delta + 16 / _Mysize));
+
+				auto y0 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - m));
+				auto y1 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - m + 16 / _Mysize));
+				auto y2 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - m + delta));
+				auto y3 = simd::sse::loadu(reinterpret_cast<const __m128i*>(s1 - m + delta + 16 / _Mysize));
+
+				__WJR_MEMRMIS_FOUR(simd::sse, 
+					s0 - m + 16 / _Mysize, 
+					s0 - m + 32 / _Mysize, 
+					s0 - m + delta + 16 / _Mysize, 
+					s0 - m + delta + 32 / _Mysize);
+			}
+#endif // WJR_AVX2
+		}
+
+		if (n <= 128 / _Mysize) {
 			return s0 - n;
 		}
-#else
-		static_assert(width * 4 == 64 / _Mysize, "");
-#endif // WJR_AVX2
+
+		s0 -= 128 / _Mysize;
+		s1 -= 128 / _Mysize;
+		n -= 128 / _Mysize;
+
+		if (n <= width * 4) {
+
+			WJR_MACRO_LABEL(unaligned_last_4vec) :
+
+			s0 -= n;
+			s1 -= n;
+
+			auto x0 = simd_t::loadu(reinterpret_cast<const sint*>(s0));
+			auto x1 = simd_t::loadu(reinterpret_cast<const sint*>(s0 + width));
+			auto x2 = simd_t::loadu(reinterpret_cast<const sint*>(s0 + width * 2));
+			auto x3 = simd_t::loadu(reinterpret_cast<const sint*>(s0 + width * 3));
+
+			auto y0 = simd_t::loadu(reinterpret_cast<const sint*>(s1));
+			auto y1 = simd_t::loadu(reinterpret_cast<const sint*>(s1 + width));
+			auto y2 = simd_t::loadu(reinterpret_cast<const sint*>(s1 + width * 2));
+			auto y3 = simd_t::loadu(reinterpret_cast<const sint*>(s1 + width * 3));
+
+			__WJR_MEMRMIS_FOUR(simd_t, s0 + width, s0 + width * 2, s0 + width * 3, s0 + width * 4);
+
+			return s0;
+		}
 
 		auto _off0 = reinterpret_cast<uintptr_t>(s0) % bound;
 		auto _off1 = reinterpret_cast<uintptr_t>(s1) % bound;
